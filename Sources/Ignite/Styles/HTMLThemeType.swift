1194893002dcb978f66d0d34d94d1abd5166b16d
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-01-06 11:35:38 +0900

    add html theme type

---
+++ Sources/Ignite/Styles/HTMLThemeType.swift
@@ -0,0 +1,14 @@
+//
+// HTMLThemeType.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+import Foundation
+
+public enum HTMLThemeType: String {
+  case light
+  case dark
+  case auto
+}
